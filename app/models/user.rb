203b4edfc82af0bc1d73e0edc770e5aef88cf72c
gurshant <gurshant53@gmail.com>
2019-11-16 14:21:57 -0800

    abilities for user are half done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_many :registrations, dependent: :destroy
   has_many :registered_cohorts, through: :registrations, source: :cohort
+  
   # has_many :blocks, dependent: :nullify
+
   has_many :attendances, dependent: :destroy
   has_many :attended_blocks, through: :attendances, source: :block
-
   has_secure_password
-  
 end
