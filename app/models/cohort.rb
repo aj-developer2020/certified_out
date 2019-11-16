d85220a128adaf5a1f06fdc4b019336ef637f2da
David Ko <davidko604@gmail.com>
2019-11-16 10:44:03 -0800

    saving work so far

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -3,4 +3,8 @@ class Cohort < ApplicationRecord
   has_many :blocks, dependent: :destroy
   has_many :registrations, dependent: :destroy
   has_many :registrants, through: :registrations, source: :user
+  
+  validates(:title, presence: true, uniqueness: true)
+
+
 end
