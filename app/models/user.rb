253d34411d1e100326441415b6ea6999d22ec526
gurshant <gurshant53@gmail.com>
2019-11-16 18:59:29 -0800

    added models and removed is_archive column from cohort

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,27 @@
 class User < ApplicationRecord
+  before_validation :set_default_is_active
+  before_validation :set_default_role
+
   has_many :registrations, dependent: :destroy
   has_many :registered_cohorts, through: :registrations, source: :cohort
-  
   # has_many :blocks, dependent: :nullify
-
   has_many :attendances, dependent: :destroy
   has_many :attended_blocks, through: :attendances, source: :block
+  
   has_secure_password
+
+
+  validates :email, presence: true, uniqueness: true,format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true, length: { minimum: 10 }
+  validates :password_digest, presence: true
+
+  def set_default_is_active
+    self.is_active ||=true
+  end
+
+  def set_default_role
+    self.role ||=0
+  end
 end
