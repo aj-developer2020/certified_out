c20acd49e0a60d9978a9a26b9e1203e0bfb62516
David Ko <davidko604@gmail.com>
2019-11-17 20:24:06 -0800

    8:25pm

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,16 +4,16 @@ class AttendancesController < ApplicationController
   # before_action :authorize_user!, only: [ :create ]
 
   def new
+    # redirect_to root_path unless can?(:index, current_user)
     @cohort = Cohort.find(params[:cohort_id])
     # @block = Block.find(params[:block_id])
-    @registrations = Registration.where(cohort_id: @cohort.id)
-    @attendances = Attendance.all
+    @registrations = Registration.where(cohort_id: @cohort.id).order(user_id: :desc)
+    # @attendances = Attendance.all
     @users = []
     for i in @registrations
       @users.push(User.find_by(id: i.user_id))
     end
-
- 
+    @blocks = Block.where(cohort_id: @cohort.id)
     # @users = User.where(user_id: @registrations.user_id)
   end
 
